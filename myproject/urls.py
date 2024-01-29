a7be488450d4dad9bb13944bfd1fe334e9934293
jeyhun <git-ceyhun071173@mail.ru>
2024-01-29 10:47:12 +0400

    Initial commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myapp/', include('myapp.urls')),
     path('',include('myapp2.urls')),
     #path('__debug__/', include("debug_toolbar.urls")),
 ]
-#pip freeze > requirements.txt
-#mysqlclient
